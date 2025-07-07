3847fb6b454948d565f72bf74714f38b2546a325
unknown <wukamlong@usp.br>
2025-07-07 02:12:53 -0300

    './TeamPage.css'

---
+++ badsite/src/data/members.js
@@ -0,0 +1,57 @@
+// src/data/membersData.js
+
+// Importe as imagens aqui. O Vite vai processá-las corretamente.
+import pedroImg from '../assets/images/team/pedro.png';
+import huaImg from '../assets/images/team/Hua.png';
+import moizesImg from '../assets/images/team/moizes.png';
+import davidImg from '../assets/images/team/takeda.png';
+import eduardoImg from '../assets/images/team/kishi.png';
+import gustavoImg from '../assets/images/team/Akira.png';
+import wuImg from '../assets/images/team/wu.jpg';
+
+const membersData = [
+  {
+    id: 1,
+    name: 'Pedro Koch Mendonça',
+    imageUrl: pedroImg,
+    description: 'Descrição... (clicar para ver mais)',
+  },
+  {
+    id: 2,
+    name: 'Hua Hua Cao',
+    imageUrl: huaImg,
+    description: 'Descrição... (clicar para ver mais)',
+  },
+  {
+    id: 3,
+    name: 'Moizés Ávila',
+    imageUrl: moizesImg,
+    description: 'Descrição... (clicar para ver mais)',
+  },
+  {
+    id: 4,
+    name: 'David Kazuo Takeda',
+    imageUrl: davidImg,
+    description: 'Descrição... (clicar para ver mais)',
+  },
+  {
+    id: 5,
+    name: 'Eduardo Kenzo Kishi',
+    imageUrl: eduardoImg,
+    description: 'Descrição... (clicar para ver mais)',
+  },
+  {
+    id: 6,
+    name: 'Gustavo Akira Hassimoto',
+    imageUrl: gustavoImg,
+    description: 'Descrição... (clicar para ver mais)',
+  },
+    {
+    id: 7,
+    name: 'Wu Kam Long',
+    imageUrl: wuImg,
+    description: 'Descrição... (clicar para ver mais)',
+  },
+];
+
+export default membersData;
