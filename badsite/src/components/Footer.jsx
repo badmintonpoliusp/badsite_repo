8d04f2945c6127e3caa12213137a21926527c881
Hua Hua Cao <huahuacao.hh@usp.br>
2025-07-07 00:08:59 -0300

    atualzilação

--- badsite/src/components/Footer.jsx
+++ badsite/src/components/Footer.jsx
@@ -2,70 +2,64 @@ import './Footer.css'
 
 function Footer() {
   return (<>
-    <div>
-        <p class = "title_bar_footer">Navegação</p>
+    <div class="container">
+        <div class="block">
+            <p class = "title_bar_footer">Navegação</p>
+            <ul>
+                <li>Home</li>
+                <li>Eventos</li>
+                <li>Blog</li>
+                <li>Galeria</li>
+                <li>Hall da Fama</li>
+                <li>Sobre</li>
+            </ul>
+        </div>
 
-        <ul>
-            <li>Home</li>
-            <li>Eventos</li>
-            <li>Blog</li>
-            <li>Galeria</li>
-            <li>Hall da Fama</li>
-            <li>Sobre</li>
-        </ul>
+        <div class="block">
+            <p class = "title_bar_footer">Parceria</p>
+            <ul>
+                <li>Patrocínios</li>
+                <li>Contribuições</li>
+                <li>Lei de Incentivo</li> 
+            </ul>
+        </div>
 
+        <div class="block">
+            <p class = "title_bar_footer">Links úteis</p>
+            <ul>
+                <li>A.A.A.P.</li>
+                <li>CEPEUSP</li>
+                <li>FEBASP</li>
+            </ul>
+        </div>
+
+        <div class="block">
+            <p class = "title_bar_footer">Mídias Sociais</p>
+            <ul>
+                <li>Instagram</li>
+                <li>Facebook</li>
+            </ul>
+        </div>
+
+        <div class="block">
+            <div>
+                <p class = "title_bar_footer">Treino e Horário</p>
+                <ul>
+                <li>Sexta: 17h às 21h</li>
+                    <li>Módlio 6 - CEPEUSP</li> 
+                </ul>
+            </div>
+
+            <div id="contact_block">
+                <p class = "title_bar_footer">Contato</p>
+                <ul>
+                    <li>bad.poliusp@gmail.com</li>
+                </ul>
+            </div>
+        </div>
     </div>
 
-    <div>
-        <p class = "title_bar_footer">Parceria</p>
-
-        <ul>
-           <li>Patrocínios</li>
-            <li>Contribuições</li>
-            <li>Lei de Incentivo</li> 
-        </ul>
-    
-    </div>
-
-    <div>
-        <p class = "title_bar_footer">Links úteis</p>
-
-        <ul>
-            <li>A.A.A.P.</li>
-            <li>CEPEUSP</li>
-            <li>FEBASP</li>
-        </ul>
-        
-    </div>
-
-    <div>
-        <p class = "title_bar_footer">Mídias Sociais</p>
-        
-        <ul>
-            <li>Instagram</li>
-            <li>Facebook</li>
-        </ul>
-        
-    </div>
-
-    <div>
-        <p class = "title_bar_footer">Treino e Horário</p>
-
-        <ul>
-           <li>Sexta: 17h às 21h</li>
-            <li>Módlio 6 - CEPEUSP</li> 
-        </ul>
-        
-    </div>
-
-    <div>
-        <p class = "title_bar_footer">Contato</p>
-
-        <ul>
-        <li>bad.poliusp@gmail.com</li>
-        </ul>
-
-    </div>
+    <div id="marco_zero"><p id="copyright">Copyright ©2025 Equipe de Badminton da Poli-Usp. Todos os direitos reservados. Desenvolvido pelo Projeto Marco Zero.</p></div>
   </>);
 }
 
