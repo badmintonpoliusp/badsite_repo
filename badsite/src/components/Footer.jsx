826560287326928f360b468448f9a2c388ce4147
Hua Hua Cao <huahuacao.hh@usp.br>
2025-07-06 22:54:34 -0300

    images

---
+++ badsite/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (<>
+    <div>
+        <p class = "title_bar_footer">Navegação</p>
+        <ul></ul>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </>);
+}
+
+export default Footer
