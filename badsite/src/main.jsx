670548c9f1410597ebaa9533c1c018f6aa5410c6
unknown <wukamlong@usp.br>
2025-07-07 02:21:47 -0300

    icon

--- badsite/src/main.jsx
+++ badsite/src/main.jsx
@@ -1,17 +1,27 @@
-// src/main.jsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Componentes
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Eventos from './components/Eventos';
+import TeamPage from './components/TeamPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+<<<<<<< Updated upstream
     <>
-      {/* <Header /> */}
+      <Header />
       <Eventos />
       <Footer />
     </>
+=======
+  <>
+    <Header />
+    <main>
+      <TeamPage />
+    </main>
+    <Footer/>
+  </>
+>>>>>>> Stashed changes
   </React.StrictMode>
 );
