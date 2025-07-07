03fa83214ef5eeedc046f9d56ac0ca39cefbc6b4
Hua Hua Cao <huahuacao.hh@usp.br>
2025-07-06 23:11:58 -0300

    atualzilação

--- badsite/src/components/Footer.jsx
+++ badsite/src/components/Footer.jsx
@@ -1,13 +1,48 @@
+import './Footer.css'
+
 function Footer() {
   return (<>
     <div>
         <p class = "title_bar_footer">Navegação</p>
-        <ul></ul>
+        <ul>Home</ul>
+        <ul>Eventos</ul>
+        <ul>Blog</ul>
+        <ul>Galeria</ul>
+        <ul>Hall da Fama</ul>
+        <ul>Sobre</ul>
     </div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+
+    <div>
+        <p class = "title_bar_footer">Parceria</p>
+        <ul>Patrocínios</ul>
+        <ul>Contribuições</ul>
+        <ul>Lei de Incentivo</ul>
+    </div>
+
+    <div>
+        <p class = "title_bar_footer">Links úteis</p>
+        <ul>A.A.A.P.</ul>
+        <ul>CEPEUSP</ul>
+        <ul>FEBASP</ul>
+    </div>
+
+    <div>
+        <p class = "title_bar_footer">Mídias Sociais</p>
+        <ul>Instagram</ul>
+        <ul>Facebook</ul>
+    </div>
+
+    <div>
+        <p class = "title_bar_footer">Treino e Horário</p>
+        <ul>Sexta: 17h às 21h</ul>
+        <ul>Módulo 6 - CEPEUSP</ul>
+    </div>
+
+    <div>
+        <p class = "title_bar_footer">Contato</p>
+        <ul>Sexta: 17h às 21h</ul>
+        <ul>Módulo 6 - CEPEUSP</ul>
+    </div>
   </>);
 }
 
--- badsite/src/main.jsx
+++ badsite/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Footer />
+    <>
+      <Header/>
+      <Footer/>
+    </>
   </StrictMode>,
 )
