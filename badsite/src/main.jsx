6df027ecd219afb6527e400ed628895d8b99149f
Hua Hua Cao <huahuacao.hh@usp.br>
2025-07-06 23:17:47 -0300

    atualzilação

--- badsite/src/main.jsx
+++ badsite/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 
