83f68d20ecce6a69f5af0e41085a16d45e20962c
unknown <wukamlong@usp.br>
2025-07-09 00:10:46 -0300

    added blog

--- badsite/src/components/SectionQuemSomos.jsx
+++ badsite/src/components/SectionQuemSomos.jsx
@@ -14,8 +14,8 @@ function SectionQuemSomos(){
             das quadras.</p>
             <p>O badminton é um esporte em crescimento na USP, conquistando cada 
             vez mais participantes interessados em sua dinâmica, técnica e diversão.</p>
-            <p>Nossos treinos são abertos a toda a comunidade USP — desde iniciantes curiosos até atletas 
-            experientes — e buscam equilibrar o desenvolvimento técnico, a integração entre alunos 
+            <p>Nossos treinos são abertos a toda a comunidade USP, desde iniciantes curiosos até atletas 
+            experientes, e buscam equilibrar o desenvolvimento técnico, a integração entre alunos 
             e colaboradores e um ambiente leve, acolhedor e colaborativo.</p>
             <p>Se você faz parte da USP e quer praticar um esporte dinâmico, conhecer gente nova e fazer parte de uma equipe engajada, 
             venha jogar com a gente! 
--- badsite/src/data/blogData.js
+++ badsite/src/data/blogData.js
@@ -1,33 +1,35 @@
 // Usaremos uma imagem de exemplo que pode estar na sua pasta de equipe
-import autorImg from '../assets/images/team/Bruno.png'; // Supondo que o nome do arquivo seja este
+import brunoImg from '../assets/images/team/Bruno.png'; // Supondo que o nome do arquivo seja este
+import henriqueImg from '../assets/images/blog/henrique.png'; // Supondo que o nome do arquivo seja este
 
 export const blogData = [
-  // {
-  //   id: 2,
-  //   slug: 'legado-da-familia-bad-poli-usp',
-  //   title: 'Além das Quadras: O Legado da Familia Bad Poli-USP',
-  //   date: '2025-07-07',
-  //   author: 'Equipe Editorial',
-  //   authorImage: null,
-  //   introQuote: 'Aqui, o badminton vai além do esporte. É união, aprendizado e a construção de uma história que nunca termina.',
-  //   content: `
-  //     <p>Este é o espaço para o conteúdo completo do post "Legado da Família".</p>
-  //     <p>Você pode detalhar aqui a história, os valores e as conquistas que formam o legado da equipe, usando múltiplos parágrafos.</p>
-  //   `
-  // },
   {
     id: 1,
-    slug: 'historia-do-badminton-minha-perspectiva',
     title: 'A história do badminton pela minha perspectiva',
     date: '2024-10-09',
     author: 'Bruno Kay Kawano',
-    authorImage: autorImg,
+    authorImage: brunoImg,
     introQuote: 'Uma jornada de desafios e conquistas que moldaram nossa modalidade na USP.',
     content: `
-      <p>A nossa como o Badminton foi fundada e é na dos minhas partes favoritas da história da modalidade. Logo que entramos na USP já queríamos fundar a modalidade, mas a ideia morreu rápido, faltou motivação...</p>
-      <p>Depois da pandemia, voltei a jogar junto com o William e um dia aleatório, do nada, virei pra ele e disse “Bora fundar o badminton?”. A gente conhece o cara que pode nos passar o contato do diretor das modalidades esportivas da Poli. Vou mandar uma mensagem pra ele agora”, e assim se abriu a modalidade na USP.</p>
-      <p>O segundo ano foi melhor significativamente, mas ainda não decolou. Começamos a ter treinos com umas 12 pessoas! O que para a gente na época já era surreal. A gente conseguiu pela primeira vez participar dos Jogos da Química! As meninas ganharam a esquerda-fio e o ano em que mais conseguimos treinar a galera e também que mais começamos a ter mais pessoas vindo toda semana.</p>
+      <p>A forma como o Badminton foi fundado é uma das minhas partes favoritas da história da modalidade. Logo que entramos na USP já queríamos fundar a modalidade, mas a ideia morreu rápido, faltou motivação. Depois da pandemia, voltei a jogar junto com o William e um dia aleatório, do nada, virei pra ele e disse “Bora fundar o badminton? A gente conhece o cara que pode nos passar o contato do diretor das modalidades esportivas da Poli. Vou mandar uma mensagem pra ele agora.”, e assim se abriu a modalidade na USP. Acho que realmente o que faltava era só esse último passo: tomar uma atitude que fez com que não tivesse mais volta, formalizar a modalidade na universidade.</p>
+      <p>O ano seguinte foi o mais desafiador da modalidade. Por quase 1 ano inteiro tivemos 4 membros da modalidade (incluindo eu e o William) e a nossa maior preocupação era “O mínimo que temos que fazer é garantir que, quando nos formarmos, o Badminton não vai acabar”. Sobrevivemos a esse ano com muita força de vontade e também com a ajuda do pessoal que já jogava Badminton na USP, na sua maioria professores e funcionários que se juntavam de 6ª para jogar. É importante mencionar também que sem eles não seria possível fundar a modalidade, porque só conseguimos jogar aproveitando o horário que eles já tinham reservado.</p>
+      <p>O segundo ano de badminton já melhorou significativamente, mas ainda não disparou. Começamos a ter treinos com até 12 pessoas! O que, para mim, já era uma grande conquista. Também foi o ano que conseguimos uma hora a mais de reserva nas quadras! As coisas começaram a esquentar. Foi o ano em que mais conseguimos treinar a galera e também quando começamos a ter mais pessoas vindo toda semana.</p>
+      <p>O terceiro ano foi quando tudo explodiu. No dia da atlética fizemos muito sucesso com nosso desafio e também por poder utilizar os módulos (quadras fechadas). Na sexta seguinte já tinham quase 20 pessoas no treino, nos meses seguintes chegamos a 30. Também foi o ano onde eu saí da cadeira de diretor de modalidade, e transferi a responsabilidade para o Pedro, o que finalmente oficializou o cumprimento da única grande meta que tínhamos quando tudo começou: quando eu sair da USP, o Bad não vai sumir, tem pessoas pra continuar puxando os treinos, e pessoas muito animadas e qualificadas também!</p>
+      <p>Agora eu estou pra me formar e já não participo tanto dos treinos, mas sei que o badminton vai continuar sendo uma modalidade relevante na USP pelo menos pelos próximos anos. E também saio com uma sensação de missão cumprida. Hoje o badminton é um dos projetos dos que mais me orgulho de ter participado, e sei que todos os treinos em que estavam só eu e o William valeram a pena no final.</p>
     `
-  }
+  },
+  {
+  id: 2,
+  title: 'Além das Quadras: O Legado da Familia Bad Poli-USP',
+  date: '2025-07-09',
+  author: 'Henrique Nassu',
+  authorImage: henriqueImg,
+  introQuote: 'Aqui, o badminton vai além do esporte. É união, aprendizado e a construção de uma história que nunca termina.',
+  content: `
+    <p>Fiz parte do time de badminton da Poli desde que ele recomeçou em 2022, por convite do William e do Bruninho, que estavam remontando a equipe que tinha sido desfeita anos antes. Participei dos primeiros projetos, de treinos físicos em horários alternativos no CEPE até torneios para conhecer mais do meio e tentar algum contato. Lembro dos dois fazendo a proposta para eu participar da fundação do time, mas eu recusei, pois já tinha me envolvido na gestão de um time anteriormente e sabia o quão difícil e estressante era a maioria do processo. Mesmo assim, sempre quis estar presente e ajudar na formação do time, mesmo que informalmente.</p>
+    <p>Desde a participação em treinos com três pessoas, em que jogávamos individuais até os braços caírem, até a entrada das primeiras pessoas, eu vi a modalidade crescer como nunca imaginava que iria acontecer, e sempre me divertindo muito, seja jogando ou conversando com as pessoas do time. Sempre fui ruim no jogo, melhorei um pouco depois de fazer uns treinos individuais, porém nunca a ponto de ganhar um jogo em um torneio. A maioria das vezes em que eu me inscrevia para jogar era principalmente para dar apoio às pessoas do time que iriam junto, seja o Bruninho, quando íamos em torneios federados só para apanhar, ou depois, quando ia mais gente, para dar carona até o local dos campeonatos.</p>
+    <p>Vendo agora que estou quase parando de jogar, porque não tenho mais tempo para isso, fico muito feliz de como o badminton cresceu e de como se tornou um ambiente acolhedor e divertido de passar o tempo. Para mim, esses foram momentos inesquecíveis e, apesar do caos, sempre foi muito divertido fazer parte da equipe, participar de projetos malucos como um lançador de petecas. No fim, acredito que fazer parte de uma equipe é aproveitar esses momentos, ver a felicidade do outro se tornando a sua, compartilhar as dificuldades e comemorações, e são coisas que vou carregar comigo para sempre.</p>
+    `
+  },
 ];
 
