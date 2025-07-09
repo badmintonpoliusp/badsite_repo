a584223447d09d928fa0cb71d58884b41aac9be4
unknown <wukamlong@usp.br>
2025-07-08 21:26:38 -0300

    Mudança CSS - Pagina Sobre

--- badsite/src/components/SectionContactInfo.jsx
+++ badsite/src/components/SectionContactInfo.jsx
@@ -1,43 +1,51 @@
 import Page_title from './Page_title';
-import './SectionContactInfo.css'
+import './SectionContactInfo.css';
 
+// 1. Importe os ícones que você vai usar
+import { FaMapMarkerAlt, FaEnvelope } from 'react-icons/fa';
 
-function SectionContactInfo(){
-    return(
-    <div>
-        <Page_title title={'Contatos e Localização'} />
-        <div className='section-container'>
-            <div className='maps-container'>
-                <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7314.362444336078!2d-46.717017!3d-23.561934!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce578fb34ff76f%3A0x496d064bde98699f!2sCEPEUSP%20-%20Centro%20de%20Pr%C3%A1ticas%20Esportivas%20da%20USP!5e0!3m2!1spt-BR!2sbr!4v1727003096886!5m2!1spt-BR!2sbr" 
-                title = 'Como chegar no CEPEUSP'
-                className='google-maps-iframe'
-                loading='lazy'
-                >
-                </iframe>
-            </div>
+function SectionContactInfo() {
+    return (
+        <div>
+            <Page_title title={'Contatos e Localização'} />
+            <div className='section-container'>
 
-            <div className='text-container'>
-
-                <div className='adress'>
-                    <h1>Endereço</h1>
-                    <p>Prç. Prof. Rubião Meira, 61</p>
-                    <p>São Paulo - SP</p>
+                {/* Bloco do Mapa com a URL correta */}
+                <div className='maps-container'>
+                    <iframe 
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.195213036583!2d-46.71993232350911!3d-23.561431378799938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce578fb34ff76f%3A0x496d064bde98699f!2sCEPEUSP%20-%20Centro%20de%20Pr%C3%A1ticas%20Esportivas%20da%20USP!5e0!3m2!1spt-BR!2sbr!4v1752018758710!5m2!1spt-BR!2sbr"
+                        title='Como chegar no CEPEUSP'
+                        className='google-maps-iframe'
+                        loading='lazy'
+                        referrerPolicy="no-referrer-when-downgrade"
+                    >
+                    </iframe>
                 </div>
 
-                <div className='contact'>
-                    <h1>Contato</h1>
-                    <p><a href="mailto:bad.poliusp@gmail.com">bad.poliusp@gmail.com</a></p>
-                </div>
+                {/* Bloco de Contato com o novo design de ícones */}
+                <div className='text-container-icons'>
+                    
+                    <div className='info-block'>
+                        <FaMapMarkerAlt size={28} className='icon' />
+                        <div>
+                            <h2>Endereço</h2>
+                            <p>Praça Prof. Rubião Meira, 61<br/>São Paulo - SP</p>
+                        </div>
+                    </div>
 
+                    <div className='info-block'>
+                        <FaEnvelope size={28} className='icon' />
+                        <div>
+                            <h2>Contato</h2>
+                            <p><a href="mailto:bad.poliusp@gmail.com">bad.poliusp@gmail.com</a></p>
+                        </div>
+                    </div>
+
+                </div>
+            
             </div>
-        
         </div>
-
-    </div>
-    
     );
 }
 
-export default SectionContactInfo
-;
+export default SectionContactInfo;
