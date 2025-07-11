dc3950bce00823f7d659dfdf6e090d4bd2ac477a
Wu <wukamlong@usp.br>
2025-07-11 04:16:12 -0300

    gallery msg added

--- badsite/src/pages/GalleryPage.jsx
+++ badsite/src/pages/GalleryPage.jsx
@@ -1,6 +1,7 @@
 // src/pages/GaleriaPage.jsx
 import ImageSlider from '../components/ImageSlider';
 import Page_title from '../components/Page_title';
+import './GalleryPage.css';
 
 function GalleryPage() {
   return (
@@ -9,6 +10,9 @@ function GalleryPage() {
       <section className="gallery-section">
         <ImageSlider source="gallery" mode="expanded" />
       </section>
+      <p className="gallery-description">
+        De treinos intensos 🏸 a pódios emocionantes 🏆, cada foto conta uma parte da nossa história de garra 💪 e paixão pelo esporte. Explore nossos melhores momentos!!! ✨
+      </p>
     </div>
   );
 }
