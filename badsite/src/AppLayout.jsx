7340d349ab838d5251b94b7a51f7b0874ea4d559
unknown <wukamlong@usp.br>
2025-07-11 02:16:46 -0300

    Gallery margin bottom added for footer spacing and scroll up added for UX

--- badsite/src/AppLayout.jsx
+++ badsite/src/AppLayout.jsx
@@ -1,23 +1,20 @@
 // src/AppLayout.jsx
-import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header'; // Verifique o caminho correto
+import Footer from './components/Footer'; // Verifique o caminho correto
+import ScrollToTop from './components/ScrollToTop'; // <-- Importe aqui
 
-function AppLayout() {
+const AppLayout = () => {
   return (
     <>
+      <ScrollToTop /> {/* <-- Adicione aqui */}
       <Header />
       <main className="main-content">
-
-        {/* Outlet renders the current route's component (e.g., HomePage, Eventos) */}
-
-        <Outlet /> 
-        
+        <Outlet /> {/* O Outlet renderiza a página da rota atual */}
       </main>
       <Footer />
     </>
   );
-}
+};
 
 export default AppLayout;
